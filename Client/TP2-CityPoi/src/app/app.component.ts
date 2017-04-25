a07adb7651ec4f80e90b7219ad38a43545ee277e
Rémikya <remikya.hellal@gmail.com>
2017-04-25 11:10:02 -0500

    Test Add markers

--- Client/TP2-CityPoi/src/app/app.component.ts
+++ Client/TP2-CityPoi/src/app/app.component.ts
@@ -9,20 +9,19 @@ export class AppComponent {
   title = 'MAP WORKS!';
   positions = [];
 
-
-  onMapReady(map) {
-    console.log('map', map);
-    console.log('markers', map.markers);  // to get all markers as an array
+  showRandomMarkers() {
+    let randomLat: number, randomLng: number;
+    this.positions = [];
+    for (let i = 0 ; i < 9; i++) {
+      randomLat = Math.random() * 0.0099 + 43.7250;
+      randomLng = Math.random() * 0.0099 + -79.7699;
+      this.positions.push([randomLat, randomLng]);
+    }
   }
-  onIdle(event){
-    console.log('map', event.target);
-  }
-  onMarkerInit(marker){
-    console.log('marker', marker);
-  }
-  onMapClick(event){
-    this.positions.push(event.latLng);
-    event.target.panTo(event.latLng);
+  addMarker() {
+    let randomLat = Math.random() * 0.0099 + 43.7250;
+    let randomLng = Math.random() * 0.0099 + -79.7699;
+    this.positions.push([randomLat, randomLng]);
   }
 
 }
