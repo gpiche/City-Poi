c565ffab38c1989878c8f5864a43cb518360fca2
Guillaume <gpiche1@yahoo.ca>
2017-05-03 12:20:02 -0400

    test

--- Client/TP2-CityPoi/src/app/CityPoi/city-search/city-search.component.ts
+++ Client/TP2-CityPoi/src/app/CityPoi/city-search/city-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Pipe} from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
@@ -9,7 +9,9 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import {CitySearchService} from './city-search.service';
-import {City} from "../../Shared/City";
+import {City} from '../../Shared/City';
+import 'rxjs/add/operator/switchMap';
+
 
 
 @Component({
@@ -21,19 +23,21 @@ import {City} from "../../Shared/City";
 export class CitySearchComponent implements OnInit {
   cities: Observable<City[]>;
   private searchTerms = new Subject<string>();
-  constructor(
-    private citySearchService: CitySearchService,
-    private router: Router) {}
+
+  constructor(private citySearchService: CitySearchService,
+              private router: Router) {
+  }
+
   // Push a search term into the observable stream.
   search(term: string): void {
     this.searchTerms.next(term);
   }
 
   ngOnInit() {
-    this.cities = this.searchTerms
+     this.cities = this.searchTerms
       .debounceTime(300)        // wait 300ms after each keystroke before considering the term
       .distinctUntilChanged()   // ignore if next search term is same as previous
-      .map(term => term   // switch to new observable each time the term changes
+      .switchMap(term => term   // switch to new observable each time the term changes
         // return the http search observable
         ? this.citySearchService.search(term)
         // or the observable of empty heroes if there was no search term
@@ -43,6 +47,7 @@ export class CitySearchComponent implements OnInit {
         console.log(error);
         return Observable.of<City[]>([]);
       });
-  }
 
+
+  }
 }
